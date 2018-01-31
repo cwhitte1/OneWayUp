23d78e8445202e9860f5e89e1f9192c4377d7401
Charles Marcus Whitten <cwhitte1@trinity.edu>
2018-01-31 15:14:19 -0600

    Mouse tracking script
    for use in unity editor

---
+++ Assets/Resources/Scripts/CameraRotationByMouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotationByMouse : MonoBehaviour
+{
+
+    public float speedH = 10.0f;
+    public float speedV = 10.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
